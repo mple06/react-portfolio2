8d0b5e4492f117f11af39804e1f3364fafc102df
Minhvan Le <mple330@gmail.com>
2022-06-29 16:20:59 -0700

    homepage complete and added links to social icons

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,8 +4,6 @@ import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -21,33 +19,34 @@ function Home2() {
             <p className="home-about-body">
               I fell in love with technology at a young age and now pursuing my passion.
               <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Python. </b>
-              </i>
+              <br /> <p className="bio">I am a former project manager turned full stack web developer. I recently earned a certificate from the<b className="purple">University of Washington Coding Program,</b> with newly developed skills in <b className="purple">Javascript, CSS, Node.js, Express, React.js and much more.</b>
+                As a <b className="purple">project manager</b>, I am a leader with strengths in communication and cultivating relationships. I have a strong ability to work independently as well as a team. I am also a strategic thinker and self-motivated individual able to manage several projects and perform under pressure in fast-paced, deadline-focused environments.
+                As a <b className="purple">web developer</b>, I am an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. Having skills in multiple facets of a business gives me a better understanding. I’m excited to leverage my skills as part of a rapid, quality-driven team to build better experiences for all.
+                Please feel free to check out my projects.</p>
               <br />
-              <br />
-              My field of Interest's are building new &nbsp;
+              My field of Interest's are &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple">creating application to improve everyday life </b> and
+                {" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  making games.
                 </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
+                <br />
+                <br />
+                <p className="bio"><span className="skillHeader"><b className="purple">Some skills</b> I bring to the table:</span>
+                  <ul className=" skillList">
+                    <li className="skill">JavaScript</li>
+                    <li className="skill">ReactJS</li>
+                    <li className="skill">HTML & CSS</li>
+                    <li className="skill">Node</li>
+                    <li className="skill">Express</li>
+                    <li className="skill">SQL</li>
+                    <li className="skill">MongoDB</li>
+                    <li className="skill">Sequelize</li>
+                    <li className="skill">Git/GitHub</li>
+                    <li className="skill">Project Management</li>
+                  </ul>
+                </p>
               </i>
             </p>
           </Col>
@@ -66,7 +65,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/minhvanle06"
+                  href="https://github.com/mple06"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
