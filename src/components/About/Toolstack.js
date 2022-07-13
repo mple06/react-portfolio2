0135fbb3792c1f0a9573c4ecd4cbe47866d1c38b
Minhvan Le <mple330@gmail.com>
2022-07-13 16:37:45 -0700

    tools and skills updated

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,30 +1,26 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiLinux,
   SiVisualstudiocode,
-  SiPostman,
   SiHeroku,
-  SiVercel,
+  SiMysql,
+  SiInsomnia,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiHeroku />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiInsomnia />
       </Col>
     </Row>
   );
